8cff4fb822391442c71621e5179373c358a60178
Bertie Blackman <bertie.blackman@airtimerewards.com>
2024-04-23 11:51:55 +0100

    adds study heating automations and config check

---
+++ shell/config_check.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+function killDocker() {
+  docker kill home-assistant >>/dev/null 2>&1
+  docker rm home-assistant >>/dev/null 2>&1
+}
+
+function copyStubbedData() {
+  cp ./.stubs/mock_secrets.yaml ./secrets.yaml
+}
+
+function cleanWorkingDirectory() {
+  rm -rf .cloud .storage .HA_VERSION home-assistant_v2.db home-assistant.log secrets.yaml
+}
+
+function checkHaConfig() {
+  killDocker
+  copyStubbedData
+
+  configCheckOutput=$(docker run --name=home-assistant -v "$PWD"/:/config homeassistant/home-assistant:stable bash -c "python -m homeassistant --script check_config --config ./ --info all")
+
+  if [[ $? == 125 ]]; then
+    exit 1
+  fi
+
+  if [[ $configCheckOutput == *"Failed config"* ]]; then
+    echo "$configCheckOutput"
+    cleanWorkingDirectory
+    killDocker
+    exit 1
+  else
+    echo "Config looks lit 🔥"
+    cleanWorkingDirectory
+    killDocker
+  fi
+}
+
+checkHaConfig
